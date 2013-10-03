ca11ade512b50a7be02294f3b9fc1587b88a66d5
Piotr Łukomiak <cieplak32@gmail.com>
2013-10-03 11:49:58 +0200

    some improvements

--- task_1/Image.h
+++ task_1/Image.h
@@ -1,12 +1,22 @@
-#include <SDL_Image.h>
+#ifndef __IMAGE_H__
+#define __IMAGE_H__
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <string>
+#include <cstdio>
 
 class Image
 {
 public:
-  Image(const char *filename);
+  Image(const std::string &filename);
   ~Image();
+
+  const SDL_Surface *get_surface();
 
 private:
   SDL_Surface *image;
 
 };
+
+#endif
