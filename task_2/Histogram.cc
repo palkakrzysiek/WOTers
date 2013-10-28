17c773ef79be9d780b8f8068668a284675e850a9
Piotr Łukomiak <cieplak32@gmail.com>
2013-10-28 18:34:55 +0100

    histogram constructor, does not work

--- task_2/Histogram.cc
+++ task_2/Histogram.cc
@@ -1,6 +1,28 @@
 #include "Histogram.h"
+#include <cstring>
+#include <cassert>
 
 Histogram::Histogram(Image &image)
 {
-  
+  int w = image.get_surface()->w;
+  int h = image.get_surface()->h;
+
+  int i, j;
+
+# pragma omp parallel for private(i)
+  for (j = 0; j < h; ++j)
+  {
+    for (i = 0; i < w; ++i)
+    {
+      uint8_t r, g, b, a;
+
+      SDL_GetRGBA(image.get_pixel(i, j), image.get_surface()->format,
+                  &r, &g, &b, &a);
+
+      ++pixels_r[r];
+      ++pixels_g[g];
+      ++pixels_b[b];
+      ++pixels_a[a];
+    }
+  }
 }
--- task_2/Histogram.h
+++ task_2/Histogram.h
@@ -2,11 +2,18 @@
 #define __HISTOGRAM_H__
 
 #include "Image.h"
+#include <map>
 
 class Histogram
 {
 public:
   Histogram(Image &image);
+
+private:
+  uint64_t pixels_r[256] = {0};
+  uint64_t pixels_g[256] = {0};
+  uint64_t pixels_b[256] = {0};
+  uint64_t pixels_a[256] = {0};
 
 };
 
