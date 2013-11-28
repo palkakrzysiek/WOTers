3cd899baa1ae6a70a7d61d3a0679fca4eb8334d7
Krzysiek Palka <palkakrzysiek@gmail.com>
2013-11-28 10:29:19 +0100

    add missing files

---
+++ src/StructuralElements.h
@@ -0,0 +1,55 @@
+#ifndef STRUCTURAL_ELEMENTS_H
+#define STRUCTURAL_ELEMENTS_H
+// vim: set ts=2 sw=2:
+enum Element {EMPTY = 0, ACTIVE = 1, INACTIVE = 2};
+enum {BLACK = 0, WHITE = 255};
+const int NUM_OF_STRUCTURAL_ELEMENTS = 9;
+const uint8_t STRUCTURAL_ELEMENT[NUM_OF_STRUCTURAL_ELEMENTS][3][3] = {
+                                             { // 0
+                                              {0, 0, 0},
+                                              {2, 1, 2},
+                                              {2, 2, 2}
+                                             },
+                                             { // 1
+                                              {2, 0, 0},
+                                              {1, 1, 0},
+                                              {1, 1, 2}
+                                             },
+                                             { // 2
+                                              {1, 2, 0},
+                                              {1, 1, 0},
+                                              {1, 2, 0}
+                                             },
+                                             { // 3
+                                              {1, 1, 2},
+                                              {1, 1, 0},
+                                              {2, 0, 0}
+                                             },
+                                             { // 4
+                                              {1, 1, 1},
+                                              {2, 1, 2},
+                                              {0, 0, 0}
+                                             },
+                                             { // 5
+                                              {2, 1, 1},
+                                              {0, 1, 1},
+                                              {0, 0, 2}
+                                             },
+                                             { // 6
+                                              {0, 2, 1},
+                                              {0, 1, 1},
+                                              {0, 2, 1}
+                                             },
+                                             { // 7
+                                              {0, 0, 2},
+                                              {0, 1, 1},
+                                              {2, 1, 1}
+                                             },
+                                             { // 8
+                                              {0, 1, 0},
+                                              {1, 1, 1},
+                                              {0, 1, 0}
+                                             }
+                                            };
+
+#endif
