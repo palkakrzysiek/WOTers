1ce0b28175c1fd213e20b08423b96718f52016f8
Krzysiek Palka <palkakrzysiek@gmail.com>
2013-12-01 07:23:37 +0100

    TADAM, the last task - region growing :metal:... :sleeping::zzz:

---
+++ src/RegionGrowing.cc
@@ -0,0 +1,78 @@
+#include "RegionGrowing.h"
+
+RegionGrowing::RegionGrowing(int s, int h)
+  : seedVal(s), thresholdMargin(h)
+{
+}
+
+void RegionGrowing::perform(Image &image)
+{
+
+  bool grayscale = (image.get_surface()->format->BitsPerPixel <= 8) ? true : false;
+  std::queue<Point> points;
+
+  if (!grayscale) {
+    image.perform_operation(new ConvertToGrayscale());
+  }
+
+  if (thresholdMargin == -1) {
+    Histogram hist(image);
+    thresholdMargin = std::abs(static_cast<long long>(seedVal - hist.cmean(Histogram::Channel::ALL)));
+  }
+  printf("marg:%d\n", thresholdMargin);
+
+  int w = image.get_surface()->w;
+  int h = image.get_surface()->h;
+
+  Image outimg(image);
+  uint8_t r, g, b;
+  int i, j;
+  for (i = 0; i < w; i++) {
+    for (j = 0; j < h; j++) {
+      SDL_GetRGB(image.get_pixel(i, j), image.get_surface()->format,
+          &r, &g, &b);
+      if (seedVal == r) {
+        Point p;
+        p.x = i, p.y = j;
+        points.push(p);
+        outimg.set_pixel(i, j, SDL_MapRGB(outimg.get_surface()->format,
+              0, 0, 0));
+      }
+
+      else {
+        outimg.set_pixel(i, j, SDL_MapRGB(outimg.get_surface()->format,
+              255, 255, 255));
+      }
+    }
+  }
+
+  printf("queue: %d\n", points.size());
+
+  while (!points.empty()) {
+    Point p = points.front();
+    points.pop();
+    for (i = p.x - 1; i <= p.x + 1; i++) {
+      for (j = p.y -1; j <= p.y + 1; j++) {
+        if ((i >= 0) && (i < w) && (j >= 0) && (j < h)) {
+          uint8_t r2, g2, b2;
+          SDL_GetRGB(outimg.get_pixel(i, j), outimg.get_surface()->format,
+              &r, &g, &b);
+          SDL_GetRGB(image.get_pixel(i, j), image.get_surface()->format,
+              &r2, &g2, &b2);
+          // if this pixel hasn't been already checked
+          // and have value within accepted range
+          if ((r != 0) && (std::abs(r2 - seedVal) <= thresholdMargin)) {
+            Point p2;
+            p2.x = i, p2.y = j;
+            points.push(p2);
+            outimg.set_pixel(i, j, SDL_MapRGB(outimg.get_surface()->format,
+                0, 0, 0));
+          }
+        }
+      }
+    }
+  }
+
+
+  image = std::move(outimg);
+}
---
+++ src/RegionGrowing.h
@@ -0,0 +1,23 @@
+#ifndef __REGION_GROWING__
+#define __REGION_GROWING__
+
+#include "Operation.h"
+#include "Histogram.h"
+#include "ConvertToGrayscale.h"
+#include <queue>
+
+struct Point {
+    int x;
+    int y;
+};
+
+class RegionGrowing : public Operation {
+    public:
+        RegionGrowing(int reg, int th);
+        void perform(Image &image);
+    private:
+        int seedVal;
+        int thresholdMargin;
+};
+
+#endif
