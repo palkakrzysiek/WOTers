4d454ba781afb8eb992c357879010386bdcc1bad
Krzysiek Palka <palkakrzysiek@gmail.com>
2014-01-08 22:08:03 +0100

    Erosion fix

--- src/Erosion.cc
+++ src/Erosion.cc
@@ -13,33 +13,29 @@ void Erosion::perform(Image &image)
 
   int i, j, k, l;
   uint8_t r, g, b;
+  bool areAllCorrect = 0;
 
-//# pragma omp parallel for private(i)
-// it does not work with parallel processing
-  for (j = 1; j < h - 1; ++j) {
-    for (i = 1; i < w - 1; ++i) {
-      SDL_GetRGB(image.get_pixel(i, j), image.get_surface()->format,
+  for (i = 1; i < w - 1; ++i) {
+    for (j = 1; j < h - 1; ++j) {
+      areAllCorrect = true;
+      workingCopy.set_pixel(i, j, SDL_MapRGB(workingCopy.get_surface()->format,
+            BG, BG, BG)); // just assume, check in next nested loops
+      for (k = -1; k <= 1; ++k) {
+        for (l = -1; l <= 1; ++l) {
+          SDL_GetRGB(image.get_pixel(i+k, j+l), image.get_surface()->format,
           &r, &g, &b);
-      //if (r == BG){
-        //workingCopy.set_pixel(i, j, SDL_MapRGB(workingCopy.get_surface()->format,
-              //BG, BG, BG)); // just assume, check in next nested loops
-      //}
-      //else {
-        //workingCopy.set_pixel(i, j, SDL_MapRGB(workingCopy.get_surface()->format,
-              //FG, FG, FG)); // just assume, check in next nested loops
-      //}
-      //printf("%d\n", r);
-      if (r == BG) {
-        for (k = -1; k <= 1; ++k) {
-          for (l = -1; l <= 1; ++l) {
-            //SDL_GetRGB(image.get_pixel(i+k, j+l), image.get_surface()->format,
-            //&r, &g, &b);
-            if (STRUCTURAL_ELEMENT[mask][k+1][l+1] == ACTIVE) {
-              workingCopy.set_pixel(i+k, j+l, SDL_MapRGB(workingCopy.get_surface()->format,
-                    BG, BG, BG));
-            }
+          if (STRUCTURAL_ELEMENT[mask][k+1][l+1] == ACTIVE && r != FG) {
+            areAllCorrect = false;
           }
         }
+      }
+
+      if (areAllCorrect) {
+        workingCopy.set_pixel(i, j, SDL_MapRGB(workingCopy.get_surface()->format,
+              FG, FG, FG));
+      } else {
+        workingCopy.set_pixel(i, j, SDL_MapRGB(workingCopy.get_surface()->format,
+              BG, BG, BG));
       }
 
     }
